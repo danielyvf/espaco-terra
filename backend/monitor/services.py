8d1a0413bac26006ef0279f98c7a0dc58b2d70ca
daniely <danielyvfl@gmail.com>
2026-07-27 12:01:32 -0400

    correção dados solares

--- backend/monitor/services.py
+++ backend/monitor/services.py
@@ -59,3 +59,40 @@ def atualizar_terremotos():
         
     except Exception as e:
         print(f"Erro ao conectar com a API do USGS: {e}")
+
+def atualizar_dados_solares():
+    """Busca os dados geomagnéticos/solares na API da NOAA"""
+    url = "https://services.swpc.noaa.gov/products/noaa-scales.json"
+    
+    try:
+        resposta = requests.get(url, timeout=10)
+        if resposta.status_code == 200:
+            dados = resposta.json()
+            
+            if '0' in dados:
+                relatorio_atual = dados['0']
+                
+                escala_g = relatorio_atual.get('Geomagnetic', {}).get('Scale', 'G0')
+                kp_estimado = float(relatorio_atual.get('Geomagnetic', {}).get('Kp', 0.0))
+                descricao_alerta = relatorio_atual.get('Geomagnetic', {}).get('Text', '')
+                
+                data_hora_real = timezone.now()
+                
+                ja_existe = EventoSolar.objects.filter(
+                    data_hora__year=data_hora_real.year,
+                    data_hora__month=data_hora_real.month,
+                    data_hora__day=data_hora_real.day,
+                    data_hora__hour=data_hora_real.hour,
+                    data_hora__minute=data_hora_real.minute
+                ).exists()
+                
+                if not ja_existe:
+                    EventoSolar.objects.create(
+                        data_hora=data_hora_real,
+                        indice_kp=kp_estimado,
+                        tipo_alerta=escala_g,
+                        descricao=descricao_alerta
+                    )
+                    print("Sucesso! Novos dados solares adicionados.")
+    except Exception as e:
+        print(f"Erro ao conectar com a API da NOAA: {e}")
