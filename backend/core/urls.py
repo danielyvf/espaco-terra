5c0d7242217c0db2e6b5cae609a203d427b5b68a
daniely <danielyvfl@gmail.com>
2026-07-28 13:11:00 -0400

    correção

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,13 @@
-# Rotas principais do servidor
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+# Função simples para responder à raiz (Health Check do Render)
+def home(request):
+    return JsonResponse({"status": "API Espaço Terra está online!"})
 
 urlpatterns = [
+    path('', home),  
     path('admin/', admin.site.urls),
-    path('api/', include('monitor.urls')), # Conecta as rotas do app monitor com o prefixo /api/
+    path('api/', include('monitor.urls')), # Conecta as rotas do app monitor
 ]
